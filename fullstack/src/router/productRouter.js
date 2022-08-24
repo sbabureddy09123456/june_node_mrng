8ad33d8e255357656945b4193bb9646dbcefafac
sbabureddy09123456 <94162906+sbabureddy09123456@users.noreply.github.com>
2022-08-24 23:28:27 +0530

    wheather

--- fullstack/src/router/productRouter.js
+++ fullstack/src/router/productRouter.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let productRouter = express.Router();
-let mongodb = require('mongodb').MongoClient;
+let mongo = require('mongodb');
+let mongodb = mongo.MongoClient;
 let url = process.env.mongoUrl;
 
 function router(menu){
@@ -50,9 +51,23 @@ productRouter.route('/category/:id')
     // res.send(products)
     // res.render('product',{title:'products Page',products,menu})
 })
-productRouter.route('/details')
+productRouter.route('/details/:id')
 .get(function(req,res){
-    res.send("products details")
+        let id = mongo.ObjectId(req.params.id)
+        mongodb.connect(url,function(err,dc){
+            if(err){
+                res.status(500).send('Error While Connecting')
+            }else{
+                let dbObj = dc.db('june8');
+                dbObj.collection('products').findOne({'_id': id},function(err,products){
+                    if(err){
+                        res.status(203).send('Error While Fetching')
+                    }else{
+                        res.render('productDetails',{title:'Products details Page',products,menu})
+                    }
+                })
+            }
+        })
 })
 
 return productRouter;
---
+++ wheatherapp/app.js
@@ -0,0 +1,27 @@
+let express = require('express')
+let request = require('request')
+let app = express();
+let port = process.env.PORT || 8700;
+
+app.use(express.static(__dirname+ '/public'))
+app.set('views','./src/views')
+app.set('view engine', 'ejs')
+
+app.get('/weather',(req,res)=>{
+    let city = req.query.city ? req.query.city :'Delhi';
+    let url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&mode=json&units=metric&cnt=10&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+    //calling api
+    request(url, (err, apiResponse)=>{
+        if(err) throw err;
+        const output = JSON.parse(apiResponse.body);
+        // res.send(output);
+        res.render('index' ,{title:'Weather App',result:output})
+    })
+
+
+})
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+    console.log(`Listen to port ${port}`)
+})
